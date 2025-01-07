c20d4bba402d4b25e319e6bf1f047107df41664d
nguyenhuyttns <huy2010200320102003@gmail.com>
2025-01-07 18:50:40 +0700

    Add search filed

--- real_project/lib/pages/home.dart
+++ real_project/lib/pages/home.dart
@@ -8,6 +8,64 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          _searchField(),
+        ],
+      ),
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.white70,
+          blurRadius: 40,
+          spreadRadius: 0.0,
+        )
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintText: 'Search Pancake',
+            hintStyle: TextStyle(
+              color: Colors.black12,
+              fontSize: 14,
+            ),
+            contentPadding: EdgeInsets.all(15),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: SvgPicture.asset('assets/icons/Search.svg'),
+            ),
+            suffixIcon: Container(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    VerticalDivider(
+                      color: Colors.white70,
+                      indent: 10,
+                      endIndent: 10,
+                      thickness: 0.1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: SvgPicture.asset('assets/icons/Filter.svg'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none,
+            )),
+      ),
     );
   }
 
